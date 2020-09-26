16ea861b946da3d6fe348b6439d8e6d9e94e9f5b
Olga <pelzolga123@gmail.com>
2020-09-26 17:31:15 +0300

    Basic project setup

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+
+};
